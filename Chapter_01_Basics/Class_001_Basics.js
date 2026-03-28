584e8d3ae0312b8cca5f0cd927a907f3b389754d
Ankit Yadav <ankit.yadav@ocacademy.in>
2026-03-28 22:58:42 +0530

    Added Some Comments

--- Chapter_01_Basics/Class_001_Basics.js
+++ Chapter_01_Basics/Class_001_Basics.js
@@ -1,10 +1,11 @@
 let result;
 
-function add(a,b){
-    return a+b;
+function add(a, b) {
+    return a + b;
 }
 
-for(let i=0;i<10;i++){
-    result=add(i,1);
+// hot code this code will got interpreter then it wiil go  profile then turbo fan meand compiler 
+for (let i = 0; i < 10000; i++) {
+    result = add(i, 1);
     console.log(result);
 }
