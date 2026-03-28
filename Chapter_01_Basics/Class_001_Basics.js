09bcd07bffcca024b8e55ccf4fb059b5abdf8bef
Ankit Yadav <ankit.yadav@ocacademy.in>
2026-03-28 21:56:19 +0530

    JavaScript first basics class

---
+++ Chapter_01_Basics/Class_001_Basics.js
@@ -0,0 +1,10 @@
+let result;
+
+function add(a,b){
+    return a+b;
+}
+
+for(let i=0;i<10;i++){
+    result=add(i,1);
+    console.log(result);
+}
---
+++ Chapter_01_Basics/SystemDetails.js
@@ -0,0 +1,9 @@
+
+// Details of platform
+console.log("system details:", process.platform);
+
+// Details of architecture
+console.log("system architecture:", process.arch);
+
+// Details of Node.js version
+console.log("Node.js version:", process.version);
